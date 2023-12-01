7c5f8beffb97a341b06cbf415589c7b933bf5945
Maryna <fortestingqa11@gmail.com>
2023-11-30 22:06:11 -0800

    moved

--- Can't_Understand_Solution.js
+++ Can't_Understand_Solution.js
@@ -25,3 +25,41 @@
 //     console.log("i = " + i);
 //     console.log("j = " + j);
 //   }
+
+
+
+let browser = "HI";   //Чому не працює?????????
+if (browser === "Edge"){
+    console.log("You've got the Edge!");
+} else if (browser === "Chrome"||"Firefox"||"Safari"||"Opera"){
+    console.log('Okay, we support this browser too');
+} else {
+    console.log('We hope this page looks ok!')
+}
+
+
+var services = {    //як тут прописати return, щоб додавав, а не склеював?
+	haircut: "60",
+	shaving: "80",
+	hair_wash: "100",
+
+    get price(){
+        return 'The total of the servises provided is ' + parseInt(this.haircut) + parseInt(this.shaving) + parseInt(this.hair_wash) + '.'
+    }
+};
+
+console.log(services.price);
+
+
+
+const services = {   //як тут прописати return, щоб додавав, а не склеював?
+    haircut: 60,
+	shaving: 80,
+	hair_wash: 100,
+
+    get price(){
+        return 'The total of the servises provided is ' + this.haircut + this.shaving + this.hair_wash + '.'
+    }
+};
+
+console.log(services.price)
